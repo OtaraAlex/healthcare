a7a6486a40aed9e63155e2e4dc64a466748f0461
OtaraAlex <mbechealex61@gmail.com>
2024-07-18 00:37:01 +0300

    feat: add success page for new appointment submission

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -7,19 +7,38 @@ import {
   DATABASE_ID,
   databases,
 } from "../appwrite.config";
-import {  parseStringify } from "../utils";
+import { parseStringify } from "../utils";
 
-export const createAppointment = async ( appointment: CreateAppointmentParams ) => {
-    try {
-      const newAppointment = await databases.createDocument(
-        DATABASE_ID!,
-        APPOINTMENT_COLLECTION_ID!,
-        ID.unique(),
-        appointment
-      );
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
 
-      return parseStringify(newAppointment);
-    } catch (error) {
-      console.error("An error occurred while creating a new appointment:", error);
-    }
-  };
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.error("An error occurred while creating a new appointment:", error);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the existing patient:",
+      error
+    );
+  }
+};
